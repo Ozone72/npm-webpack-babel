b61b9f89e3381cf38823817f63f0b110078046c1
Orin Fletcher <Ozone72@users.noreply.github.com>
2017-12-19 19:18:14 -0800

    first component, yarn.lock

---
+++ app/index.js
@@ -0,0 +1,11 @@
+let React = require("react");
+let ReactDOM = require("react-dom");
+require("./index.css");
+
+class App extends React.Component {
+  render() {
+    return <div>Hello World!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
