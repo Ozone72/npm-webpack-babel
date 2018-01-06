cbb9cdbe7412d785868bef32074f20d359bcffa6
Orin Fletcher <orinf@yahoo.com>
2018-01-05 18:16:26 -0800

    finished the Popular component: added state and inline styling onClick

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,18 +1,37 @@
 var React = require('react');
 
 class Popular extends React.Component{
+    constructor (props){
+        super(props);
+        this.state = {
+            selectedLanguage: 'All'
+        };
+        // don't quite get the bind thing
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+    updateLanguage(lang){
+        this.setState(function(){
+            return{
+                selectedLanguage: lang
+            }
+        });
+    }
     render(){
         let languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-
+        // console.log('Up Here!', this);
         return (
             <ul className = 'languages'>
                 {languages.map(function(lang){
+                    // console.log('Down Here!', this);
                     return (
-                        <li>
+                        <li 
+                            style = {lang===this.state.selectedLanguage ? {color: 'red'}:null}
+                            onClick = {this.updateLanguage.bind(null, lang)}
+                            key={lang}>
                             {lang}
                         </li>
                     )
-                })}
+                }, this)}
             </ul>
         )
     }
