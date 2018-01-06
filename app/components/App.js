0bfab5d8de6e3603cb981581443eac79492bb698
Orin Fletcher <orinf@yahoo.com>
2018-01-05 17:23:31 -0800

    created Popular.js

--- app/components/App.js
+++ app/components/App.js
@@ -1,8 +1,12 @@
 var React = require('react');
+var Popular = require('./Popular');
 
 class App extends React.Component {
     render() {
-      return <div>Hello World!</div>;
+      return 
+      <div>
+        <Popular />
+      </div>;
     }
   }
 
