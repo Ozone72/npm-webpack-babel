bd57b8179096303e4a2a30336881bbad9f40476a
Orin Fletcher <orinf@yahoo.com>
2018-01-05 16:51:10 -0800

    created: App.js and components folder

--- app/index.js
+++ app/index.js
@@ -1,11 +1,7 @@
 let React = require("react");
 let ReactDOM = require("react-dom");
 require("./index.css");
+let App = require('./components/App');
 
-class App extends React.Component {
-  render() {
-    return <div>Hello World!</div>;
-  }
-}
-
+// ========= RENDER SECTION ============ //
 ReactDOM.render(<App />, document.getElementById("app"));
